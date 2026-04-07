09e7fa02d0d8a99e4f9162eb7074380e1d5ab928
Nils Nieuwejaar <nils@oxidecomputer.com>
2026-04-07 14:04:20 -0400

    fmt check

--- regs/build.rs
+++ regs/build.rs
@@ -3,11 +3,9 @@ use std::{env, fs, path::Path};
 use rsf::rust_codegen::AddrType;
 
 fn main() {
-    let code = rsf::rust_codegen::codegen(
-        "../rsf/tf2.rsf".into(),
-        AddrType::U32,
-    )
-    .unwrap();
+    let code =
+        rsf::rust_codegen::codegen("../rsf/tf2.rsf".into(), AddrType::U32)
+            .unwrap();
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("tf2_rpi.rs");
